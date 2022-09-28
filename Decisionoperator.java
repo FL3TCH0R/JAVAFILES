07dd08426649278ab26d51b6c2934585e49444fe
FL3TCH0R <114429899+FL3TCH0R@users.noreply.github.com>
2022-09-28 12:55:37 -0400

    Add files via upload

---
+++ Decisionoperator.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.decisionoperator;
+
+/**
+ *
+ * @author fletc
+ */
+public class Decisionoperator {
+
+    public static void main(String[] args) {
+        
+        
+        char c1='s', c2='y';
+        if(c1>c2)
+        {
+           System.out.println("2nd number is less than 1st number");
+           System.out.println("1st number is greater");
+        }
+        else
+        {
+            System.out.println("2nd number is greater");
+        }
+        
+        
+
+    }
+    
+
+}
+        
+        
+        
+        
+        
+    
+
